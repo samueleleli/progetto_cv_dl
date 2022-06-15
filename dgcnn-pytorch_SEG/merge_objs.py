468830bcf5ecfe3568fec0746ac36979f012c417
Lisa <lisaburini.98@gmail.com>
2022-06-15 16:14:27 +0200

    merge oggetti synthcity

---
+++ dgcnn-pytorch_SEG/merge_objs.py
@@ -0,0 +1,25 @@
+import glob
+import numpy as np
+
+# cd /Users/Lisa/Documents/Visual_Studio_Code/progetto_cv_dl/dgcnn-pytorch_SEG/data/sinthcity-npy/sinthcity
+
+objs = []
+sorted_objs = sorted(glob.glob("*.npy"))
+for area in sorted_objs:
+    obj = np.load(area)
+    objs = np.append(objs, obj)
+    print(area)
+
+np.save('merge.npy',objs)
+
+
+
+
+# -------------
+# a = np.load('a.npy')
+# b = np.load('b.npy')
+# c = []
+
+# c = np.append(a,b)
+
+# np.save('merge.npy',c)
