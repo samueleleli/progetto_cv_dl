c5d3295851f0fddd3307eb307f75049b74cb5364
MATTEO FERRETTI <S1083630@studenti.univpm.it>
2022-06-22 12:57:24 +0200

    modifica creazione dir per salvataggio risultati

--- dgcnn-pytorch_PROGETTO/configSettings.py
+++ dgcnn-pytorch_PROGETTO/configSettings.py
@@ -12,16 +12,16 @@ DATASET_LABS = "data/synthcity/labs_Area_3.txt" # non più utilizzato per il tes
 MODEL_PATH = "models/model_3_acc.t7" # models/model.t7  - modello allenato (su aree 1,24,5,6,7,8,9 di synthcity) all'epoca con maggior accuratezza 
 MODEL = 'dgcnn_semseg' # architettura utilizzata: dgcnn per semantic segmentation
 OUTPUT_CHANNELS = 9 # synthcity ha 9 classi
-EXP_DIR = "test_output" # directory dove salvare output
 TEST_DATASET = "synthcity" # dataset da predire (facciamo predizione su area 3)
 TEST_AREA = '3'
-TEST_BATCH_SIZE = 1 # provare anche 2 / 4 / 16 / 32 / 64 / 128
+TEST_BATCH_SIZE = 1 # provare anche 2 / 4 / 8 / 16 / 32 / 64 
+NUM_POINTS = 4096 # cosa indica? l'intorno da considerare per convoluzione? # provare anche 4096 / 2048 / 1024 / 512 
 MODEL_ROOT = "models"
 PARALLEL = True
-NUM_POINTS = 4096 # cosa indica? l'intorno da considerare per convoluzione?
 SEED = 1 # serve per pytorch CUDA
 EXTRACT = False # serve nel main sem_seg per far partire il codice extract
 EVAL = True # serve nel main sem_seg per far partire il codice di test
+EXP_DIR = "test_output_np" + str(NUM_POINTS) + "_bs" + str(TEST_BATCH_SIZE) # directory dove salvare output
 UNIQUE_RESULTS = False # serve per scegliere se sovrascrivere ogni volta il file txt dei risultati o creare ogni volta un nuovo file
 
 
